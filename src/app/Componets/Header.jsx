71d00d7b2ed91201c2b434b9caedea8ba048fd23
Harsh24082001 <chintuvishwa2001@gmail.com.com>
2025-10-28 21:13:31 +0530

    Update header Errors

--- src/app/Componets/Header.jsx
+++ src/app/Componets/Header.jsx
@@ -25,8 +25,8 @@ const Header = () => {
 
                 {/* Desktop Menu */}
                 <ul className="hidden md:flex justify-center md:w-full items-center gap-10 text-base font-bold">
-                    <a href="/photography"><li className={`${playfair.className} hover:text-[#e9c08b] cursor-pointer transition`}>PHOTOGRAPHY</li></a>
-                    <a href="/videography"><li className={`${playfair.className} hover:text-[#e9c08b] cursor-pointer transition`}>FILMS</li></a>
+                    <a href="/photo"><li className={`${playfair.className} hover:text-[#e9c08b] cursor-pointer transition`}>PHOTOGRAPHY</li></a>
+                    <a href="/video"><li className={`${playfair.className} hover:text-[#e9c08b] cursor-pointer transition`}>FILMS</li></a>
                     <a href="/"><img src="/logo.png" alt="Logo" className="w-32 sm:w-80 object-contain"/></a>
                     <a href="/contact-us"><li className={`${playfair.className} hover:text-[#e9c08b] cursor-pointer transition`}>CONTACT US</li></a>
                     <a href="/about-us"><li className={`${playfair.className} hover:text-[#e9c08b] cursor-pointer transition`}>ABOUT</li></a>
@@ -36,8 +36,8 @@ const Header = () => {
             {/* Mobile Dropdown Menu */}
             {menuOpen && (
                 <ul className="flex flex-col items-center bg-white text-gray-700 py-4 space-y-4 md:hidden shadow-md">
-                    <a href="/photography"><li className="hover:text-black cursor-pointer transition">Photography</li></a>
-                     <a href="/videography"><li className="hover:text-black cursor-pointer transition">Films</li></a>
+                    <a href="/photo"><li className="hover:text-black cursor-pointer transition">Photography</li></a>
+                     <a href="/video"><li className="hover:text-black cursor-pointer transition">Films</li></a>
                     <a href="/contact-us"><li className="hover:text-black cursor-pointer transition">Contact Us</li></a>
                     <a href="/about-us"><li className="hover:text-black cursor-pointer transition">About</li></a>
                 </ul>
